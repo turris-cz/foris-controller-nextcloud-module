b57d8ee1d35526d65fccdc9b841519192bf8818b
Stepan Henek <stepan.henek@nic.cz>
2023-08-22 10:54:10 +0200

    CI: start using tox and publish python packages into registry

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #
 # foris-controller-nextcloud-module
-# Copyright (C) 2020 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
+# Copyright (C) 2023 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -44,18 +44,9 @@ setup(
     license='GPL-3.0-only',
     description=DESCRIPTION,
     long_description=open("README.rst").read(),
-    install_requires=[
-        "foris-controller @ git+https://gitlab.nic.cz/turris/foris-controller/foris-controller.git"
-    ],
-    setup_requires=[
-        "pytest-runner",
-        "flake8<4.0",  # setuptools integration with flake8 is deprecated in version >=4.0
-    ],
+    install_requires=["foris-controller"],
+    setup_requires=["pytest-runner"],
     tests_require=["pytest", "foris-controller-testtools", "foris-client", "ubus", "paho-mqtt"],
-    dependency_links=[
-        "git+https://gitlab.nic.cz/turris/foris-controller/foris-controller-testtools.git#egg=foris-controller-testtools",
-        "git+https://gitlab.nic.cz/turris/foris-controller/foris-client.git#egg=foris-client",
-    ],
     include_package_data=True,
     zip_safe=False,
 )
